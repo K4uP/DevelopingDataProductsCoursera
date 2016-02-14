f7dbef47c893d0aa2736bb86a7cf90771c34e4a8
Rene Kaup <rene.kaup@gmail.com>
2016-02-14 19:30:59 +0100

    Almost finished
    Almost finished

---
+++ server.R
@@ -0,0 +1,40 @@
+library(shiny)
+library(ggplot2)
+
+shinyServer(
+  function(input,output)
+  {
+    #Barplot Colnames
+    barnames <- c("Length", "Breadth", "Height", "Volume", "Surface")
+    
+    #Volume
+    output$oid1<-renderText({(as.numeric((input$length))*as.numeric((input$breadth))*as.numeric((input$height)))})
+    #Surface
+    output$oid2<-renderText({(as.numeric(input$length)*as.numeric(input$breadth)+
+        as.numeric(input$height)*as.numeric(input$breadth)+
+        as.numeric(input$length)*as.numeric(input$height))})
+    #Date
+    output$odate<-renderPrint({input$date})
+    #Plot
+    output$plot <- renderPlot({
+          
+          barplot(height = cbind(as.numeric(input$length),
+                                 as.numeric(input$breadth),
+                                 as.numeric(input$height),
+                                 as.numeric((input$length))*as.numeric((input$breadth))*as.numeric((input$height)),
+                                 as.numeric(input$length)*as.numeric(input$breadth)+
+                                  as.numeric(input$height)*as.numeric(input$breadth)+
+                                  as.numeric(input$length)*as.numeric(input$height)
+                                 ),
+                  width = 1, space = NULL,
+                  names.arg = barnames, legend.text = NULL, beside = FALSE,
+                  horiz = FALSE, density = NULL, angle = 45,
+                  col = NULL, border = par("fg"),
+                  main = "Cube", sub = NULL, xlab = 'Dimensions', ylab = 'Value',
+                  xlim = NULL, ylim = NULL, xpd = TRUE, log = "",
+                  axes = TRUE, axisnames = TRUE,
+                  cex.axis = par("cex.axis"), cex.names = par("cex.axis"),
+                  inside = TRUE, plot = TRUE, axis.lty = 0, offset = 0,
+                  add = FALSE, args.legend = NULL)
+    })
+  })
---
+++ ui.R
@@ -0,0 +1,43 @@
+shinyUI(fluidPage(
+  headerPanel("Cuboid Calculator"),
+  sidebarLayout(
+  sidebarPanel(
+    
+    numericInput(inputId='length','Enter Length', value='0'),
+    numericInput(inputId='breadth','Enter Breadth', value='0'),
+    numericInput(inputId='height','Enter Height', value='0'),
+    dateInput("date","Date:")
+    
+  ),
+  mainPanel(
+    tabsetPanel(
+      tabPanel('Calculate',
+        h2('Volume and Surface Area'),
+                h4('Volume is'),
+                verbatimTextOutput("oid1"),
+                h4('Surface Area is'),
+                verbatimTextOutput("oid2"),
+                h4('Today Date is'),
+                verbatimTextOutput("odate")),
+      tabPanel('Plot',
+               plotOutput(outputId = "plot", height = "400px")),
+                
+      tabPanel('Documentation',
+              h2('Cuboid Calculator'),
+              h4('This application allows the user to enter the length, breadth, 
+                  and height to get the volume and surface area of cuboid.'),
+              h2('Course Project: Shiny Application and Reproducible Pitch'),
+              h4('About this Course'),
+              h4('A data product is the production output from a statistical 
+                 analysis. Data products automate complex analysis tasks or 
+                 use technology to expand the utility of a data informed model, 
+                 algorithm or inference. This course covers the basics of 
+                 creating data products using Shiny, R packages, and interactive 
+                 graphics. The course will focus on the statistical fundamentals 
+                 of creating a data product that can be used to tell a story 
+                 about data to a mass audience.'),
+              h2('Reproducible Pitch Presentation'),
+              h4('My presentation is published here:'),
+              h4('....')
+              )
+  )))))
